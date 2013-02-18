309eaf686b4c4accd4e6f838806443c3addbfadf
Vinod Dhomse <vinod@billfloat.com>
2013-02-18 09:13:06 -0800

    pushing all files and folders for the first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1cf78473a5c80704bf975e206c574ce4d0edfd425f256cfd65a8b6a851ce70f1f5dbd5d6cff18131aa6d5ace93ee94a6113924064fc639b78a58e4f42b839dbb'
